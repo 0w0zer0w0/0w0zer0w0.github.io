90f60e60baf05b43decac9c8ef0e9654988dbadd
Camilo Martinez <99211832+0w0zer0w0@users.noreply.github.com>
2025-02-07 22:39:52 -0500

    dist packaging

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
