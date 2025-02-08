a5a3de0a06d6d3f851a4d18cf04ed4e76a694cc0
Camilo Martinez <99211832+0w0zer0w0@users.noreply.github.com>
2025-02-08 17:09:44 -0500

    Update Carousel.jsx

--- components/Hero/Carousel.jsx
+++ components/Hero/Carousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 const InfiniteScroll = () => {
-  const basePath = process.env.NODE_ENV === "production" ? "/Portfolio" : "";
+  const basePath = "/Portfolio";
   const icons = [
     {
       name: "HTML",
