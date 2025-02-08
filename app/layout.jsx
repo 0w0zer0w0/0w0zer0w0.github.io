a420adad767b48bfc368921a2282e76ffa3f0aef
Camilo Martinez <99211832+0w0zer0w0@users.noreply.github.com>
2025-02-07 22:43:10 -0500

    fix mouseover

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import "@styles/globals.css";
 import Navbar from "@components/Navbar";
-import Mouseover from "@components/mouseover";
+import Mouseover from "@components/Mouseover";
 import Footer from "@components/Footer";
 
 export const metadata = {
